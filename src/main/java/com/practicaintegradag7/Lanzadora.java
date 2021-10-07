7552728d53f9612af04f40bd437ce39b51c36cec
Arturo <arturoguijarro@hotmail.com>
2021-10-07 15:51:36 +0200

    first commit

---
+++ src/main/java/com/practicaintegradag7/Lanzadora.java
@@ -0,0 +1,12 @@
+package com.practicaintegradag7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lanzadora {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lanzadora.class, args);
+	}
+}
