cfe21d2fafe378a17cad23200ada72626a9c5f91
Arturo <arturoguijarro@hotmail.com>
2021-10-19 17:39:49 +0200

    created Centro and TestCentro

---
+++ src/main/java/com/practicaintegradag7/model/Centro.java
@@ -0,0 +1,66 @@
+package com.practicaintegradag7.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class Centro {
+	
+	@Id
+	@GeneratedValue(generator = "uuid")
+	@GenericGenerator(name = "uuid", strategy = "uuid2")
+	private String id;
+	
+	@Column(name = "nombre")
+	private String nombre;
+	
+	@Column(name = "direccion")
+	private String direccion;
+	
+	@Column(name = "vacunasDisponibles")
+	private int vacunasDisponibles;
+	
+	@OneToMany(
+			mappedBy = "centro",
+			cascade = { CascadeType.PERSIST, CascadeType.MERGE },
+			fetch = FetchType.EAGER
+	)
+	private Set<Cita> centroCitas = new HashSet<>();
+	
+	public Centro(String nombre, String direccion, int vacunas) {
+		
+		if(vacunas < 0) {
+			throw new IllegalArgumentException("vacunas is not valid");
+		}
+		
+		this.nombre = nombre;
+		this.direccion = direccion;
+		this.vacunasDisponibles = vacunas;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+	
+	public int getVacunasDisponibles() {
+		return vacunasDisponibles;
+	}
+	
+	public Set<Cita> getCentroCitas() {
+		return centroCitas;
+	}
+}
---
+++ src/test/java/com/practicaintegradag7/model/TestCentro.java
@@ -0,0 +1,32 @@
+package com.practicaintegradag7.model;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TestCentro {
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void failIfVacunasNotValid() {
+		new Centro("Centro 1", "Calle 1", -2);
+	}
+	
+	@Test
+	public void failIfNombreNotEquals() {
+		Centro centro = new Centro("Centro 2", "Calle 2", 0);
+		assertEquals("Centro 2", centro.getNombre());
+	}
+	
+	@Test
+	public void failIfDireccionNotEquals() {
+		Centro centro = new Centro("Centro 1", "Calle 1", 1);
+		assertEquals("Calle 1", centro.getDireccion());
+	}
+	
+	@Test
+	public void failIfVacunasDisponiblesNotEquals() {
+		Centro centro = new Centro("Centro 1", "Calle 3", 100);
+		assertEquals(100, centro.getVacunasDisponibles());
+	}
+
+}
