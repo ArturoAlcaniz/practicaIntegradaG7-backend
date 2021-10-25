87a15a09a09bc456dfc20512403b0598f0359813
Rubén Moraleda Sánchez <ruben.moraleda@alu.uclm.es>
2021-10-25 11:47:51 +0200

    Fix code smells

--- src/main/java/com/practicaintegradag7/repos/UsuarioRepository.java
+++ src/main/java/com/practicaintegradag7/repos/UsuarioRepository.java
@@ -1,7 +1,5 @@
 package com.practicaintegradag7.repos;
 
-import java.util.List;
-
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
