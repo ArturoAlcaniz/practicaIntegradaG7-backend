b2f56709dfd23a8085833ad3a991482e5db103dc
Pablo <Pablo.Guerrero@alu.uclm.es>
2021-10-25 13:19:34 +0200

    Añadir rama a Build.yml

--- src/test/java/com/practicaintegradag7/model/TestUsuario.java
+++ src/test/java/com/practicaintegradag7/model/TestUsuario.java
@@ -11,7 +11,7 @@ public class TestUsuario {
 	
 	@Test(expected = IllegalArgumentException.class)
 	public void checkValidationDni() {
-		new Usuario("0234", "Roberto", "Brasero Hidalgo", "robertoBrasero@a3media.es", "Iso+grupo7", new Centro("Centro 1", "Calle 1", 10),
+		new Usuario("0234M", "Roberto", "Brasero Hidalgo", "robertoBrasero@a3media.es", "Iso+grupo7", new Centro("Centro 1", "Calle 1", 10),
 				"paciente");
 	}
 	
