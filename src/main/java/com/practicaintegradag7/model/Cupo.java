91d9b70fdda829bbc9ec590a80c291d0667f0bc5
Rubén Moraleda Sánchez <ruben.moraleda@alu.uclm.es>
2021-10-25 11:48:16 +0200

    Update Cupo.java

--- src/main/java/com/practicaintegradag7/model/Cupo.java
+++ src/main/java/com/practicaintegradag7/model/Cupo.java
@@ -3,7 +3,6 @@ package com.practicaintegradag7.model;
 import java.time.LocalDateTime;
 
 import javax.persistence.Column;
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
