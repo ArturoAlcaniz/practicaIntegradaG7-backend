6d456bf4776460229d2eae4e93db10f88833140d
beatr <beatr@DESKTOP-GRTSOST.(none)>
2021-10-26 20:12:46 +0200

    Cambio en el path por path="api/centro/create"

--- src/main/java/com/practicaintegradag7/controllers/CentroController.java
+++ src/main/java/com/practicaintegradag7/controllers/CentroController.java
@@ -33,7 +33,7 @@ public class CentroController {
 		aux.addVacunas(c.getId(), amount);
 	}
 	
-	@PostMapping("centros/create")
+	@PostMapping(path="api/centros/create")
 	public Centro crearCentro(@RequestBody Map<String, Object> datosCentro) throws JSONException, CentroExistException, CentroNotFoundException{
 		JSONObject jso = new JSONObject(datosCentro);
 		String nombre = jso.getString("nombre");
