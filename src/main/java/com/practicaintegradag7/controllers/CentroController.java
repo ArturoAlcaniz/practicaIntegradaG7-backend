dbe48c1ec2a42442bbee30f688928a9f2912073e
beatr <beatr@DESKTOP-GRTSOST.(none)>
2021-10-28 19:15:22 +0200

    Línea

--- src/main/java/com/practicaintegradag7/controllers/CentroController.java
+++ src/main/java/com/practicaintegradag7/controllers/CentroController.java
@@ -39,7 +39,6 @@ public class CentroController {
 		aux.addVacunas(c.getId(), amount);
 	}
 	
-	
 	@PostMapping(path="api/centros/create")
 	public Centro crearCentro(@RequestBody Map<String, Object> datosCentro) throws JSONException, CentroExistException, CentroNotFoundException{
 		JSONObject jso = new JSONObject(datosCentro);
