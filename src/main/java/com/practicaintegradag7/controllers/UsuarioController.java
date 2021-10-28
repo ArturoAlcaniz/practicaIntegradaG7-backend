4df848574be3fe22eb84e41da31c54b03b23fb84
Ismael Martín-Portugués Villegas <32538380+puzzismael@users.noreply.github.com>
2021-10-28 21:45:33 +0200

    Creaccion del post y el get usuario  en el backend

--- src/main/java/com/practicaintegradag7/controllers/UsuarioController.java
+++ src/main/java/com/practicaintegradag7/controllers/UsuarioController.java
@@ -3,9 +3,14 @@ package com.practicaintegradag7.controllers;
 import java.util.Map;
 
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.practicaintegradag7.model.Centro;
 import com.practicaintegradag7.model.Usuario;
@@ -15,6 +20,10 @@ import com.practicaintegradag7.repos.CentroRepository;
 @RestController
 public class UsuarioController {
 	
+	@Autowired
+	private Usuario user;
+	
+	@PostMapping(path="api/usuario/create")
 	public Usuario crearUsuario(@RequestBody Map<String, Object> datosUsuario) {
 		JSONObject jso = new JSONObject(datosUsuario);
 		String dni = jso.getString("dni");
@@ -24,8 +33,16 @@ public class UsuarioController {
 		String password = jso.getString("password");
 		Centro centro = CentroRepository.findByNombre(jso.getString("centro"));
 		String rol = jso.getString("rol");
-		Usuario user= new Usuario(dni, nombre, apellidos, email, password, centro, rol);
+		Usuario useri= new Usuario(dni, nombre, apellidos, email, password, centro, rol);
 		return user;
 	}
-
+	
+	@GetMapping(path="api//obtener")
+	public boolean obtenerUsuario(){
+		try {
+			return user.isPrimeraDosis();
+		} catch (Exception e) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+		}
+	}
 }
