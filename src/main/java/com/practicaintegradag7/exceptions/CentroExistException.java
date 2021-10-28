03dd87df4be4d20eda9d1b45e224b6711e1a9e5b
beatr <beatr@DESKTOP-GRTSOST.(none)>
2021-10-28 19:24:42 +0200

    Merge Conflict

--- src/main/java/com/practicaintegradag7/exceptions/CentroExistException.java
+++ src/main/java/com/practicaintegradag7/exceptions/CentroExistException.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.practicaintegradag7.exceptions;
 
 public class CentroExistException  extends Exception{
@@ -16,22 +15,3 @@ public class CentroExistException  extends Exception{
 	}
 	
 }
-=======
-package com.practicaintegradag7.exceptions;
-
-public class CentroExistException  extends Exception{
-	
-	private static final long serialVersionUID = 4L;
-	private final String mssg;
-
-	public CentroExistException (String mssg) {
-		this.mssg = mssg;
-	}
-	
-	@Override
-	public String getMessage() {
-		return mssg;
-	}
-	
-}
->>>>>>> 5683872b2f93a3b77cda0575e90f3ff9b1a12075
