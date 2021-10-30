6b22992863052b4251d77135bbc2c85092a439c4
Ismael Martín-Portugués Villegas <32538380+puzzismael@users.noreply.github.com>
2021-10-30 11:03:30 +0200

    Creado el Contoller de cupos

---
+++ src/main/java/com/practicaintegradag7/controllers/CuposController.java
@@ -0,0 +1,35 @@
+package com.practicaintegradag7.controllers;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.practicaintegradag7.dao.CentroDao;
+import com.practicaintegradag7.exceptions.CentroNotFoundException;
+import com.practicaintegradag7.model.Centro;
+import com.practicaintegradag7.model.Cupo;
+
+public class CuposController {
+	@Autowired
+	private final CentroDao aux= new CentroDao();
+	
+
+	@PostMapping(path="api/cupo/create")
+	public Cupo crearCupos(@RequestBody Map<String, Object> datosCupo) throws JSONException, CentroNotFoundException {
+		JSONObject jso = new JSONObject(datosCupo);
+		String fechaini =  jso.getString("fecha fin");
+		LocalDateTime fechainicio = LocalDateTime.parse(fechaini);
+		String fecha2 = jso.getString("fecha fin");
+		LocalDateTime fechafin = LocalDateTime.parse(fecha2);
+		int numcitas = jso.getInt("numero citas");
+		Centro centro = aux.buscarCentroByNombre(jso.getString("centro"));
+		Cupo cupo= new Cupo(fechainicio, fechafin, numcitas, centro);
+		return cupo;
+		}
+
+}
