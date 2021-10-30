cc7701770653ca4f8265a3f59ae660273565d570
AARÓN BARRAGÁN GÓMEZ <Aaron.Barragan@alu.uclm.es>
2021-10-30 15:03:35 +0000

    Updated CentroRepository.java

--- src/main/java/com/practicaintegradag7/repos/CentroRepository.java
+++ src/main/java/com/practicaintegradag7/repos/CentroRepository.java
@@ -1,20 +1,16 @@
 package com.practicaintegradag7.repos;
 
 import java.io.Serializable;
+import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.practicaintegradag7.model.Centro;
 
 @Repository
-@CrossOrigin(value = {})
 public interface CentroRepository extends MongoRepository<Centro, Serializable>{
-
-	static Centro findByNombre(String string) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
+	@Query("{ 'nombre': ?0}")
+	Optional<Centro> findByNombre(String centro);
 }
