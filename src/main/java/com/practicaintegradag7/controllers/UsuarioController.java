ff2f168f72698d6e488fdbf8089369ea607066c9
AARÓN BARRAGÁN GÓMEZ <Aaron.Barragan@alu.uclm.es>
2021-10-30 15:43:14 +0000

    Updated UsuarioController.java

--- src/main/java/com/practicaintegradag7/controllers/UsuarioController.java
+++ src/main/java/com/practicaintegradag7/controllers/UsuarioController.java
@@ -3,6 +3,7 @@ package com.practicaintegradag7.controllers;
 import java.util.Map;
 
 import org.json.JSONObject;
+import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
+import com.practicaintegradag7.exceptions.CentroNotFoundException;
+import com.practicaintegradag7.exceptions.CifradoContrasenaException;
 import com.practicaintegradag7.dao.UsuarioDao;
 import com.practicaintegradag7.dao.CentroDao;
 import java.util.List;
