52fba927fbee6b7a880b1725c620140328a1105e
Rubén Moraleda Sánchez <ruben.moraleda@alu.uclm.es>
2021-11-06 09:38:06 +0100

    Fixed

--- src/main/java/com/practicaintegradag7/controllers/AppointmentController.java
+++ src/main/java/com/practicaintegradag7/controllers/AppointmentController.java
@@ -1,13 +1,17 @@
 package com.practicaintegradag7.controllers;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.practicaintegradag7.dao.CitaDao;
 import com.practicaintegradag7.exceptions.CitasCupoNotAvailable;
 import com.practicaintegradag7.exceptions.CitasUsuarioNotAvailable;
+import com.practicaintegradag7.model.Cita;
 import com.practicaintegradag7.model.LDTFormatter;
+
+import java.util.List;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -26,5 +30,10 @@ public class AppointmentController{
 		response.put("message", "Ha pedido cita correctamente para el "+LDTFormatter.processLDT(citaDao.createCita().getFecha()));
     	return response.toString();
     }
+	
+	@GetMapping(path="/api/citas/obtener")
+	public List<Cita> obtenerCitas(){
+		return citaDao.getAllCitas();
+	}
 }
 
