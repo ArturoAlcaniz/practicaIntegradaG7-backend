c669c91b1f48f8b97ae8d90b940de885a31ccb57
Arturo <arturoguijarro@hotmail.com>
2021-11-06 02:47:05 +0100

    Test created for Configuration

---
+++ src/test/java/com/practicaintegradag7/model/TestConfiguration.java
@@ -0,0 +1,28 @@
+package com.practicaintegradag7.model;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+public class TestConfiguration {
+
+	@Test
+	void failWhenHoraInicioNotEquals() {
+		fail("Not implemented");
+	}
+	
+	@Test
+	void failWhenHoraFinNotEquals() {
+		fail("Not implemented");
+	}
+	
+	@Test
+	void failWhenCitasPorFranjaNotEquals() {
+		fail("Not implemented");
+	}
+	
+	@Test
+	void failWhenFranjasPorDiaNotEquals() {
+		fail("Not implemented");
+	}
+}
