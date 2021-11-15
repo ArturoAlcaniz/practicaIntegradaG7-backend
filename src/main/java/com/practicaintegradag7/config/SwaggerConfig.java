889e8fe60f7a62553f3073f192697dd7fce2fb0a
Rubén Moraleda Sánchez <ruben.moraleda@alu.uclm.es>
2021-11-15 11:52:32 +0100

    Update SwaggerConfig.java

--- src/main/java/com/practicaintegradag7/config/SwaggerConfig.java
+++ src/main/java/com/practicaintegradag7/config/SwaggerConfig.java
@@ -1,10 +1,13 @@
 package com.practicaintegradag7.config;
 
+
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -20,6 +23,17 @@ public class SwaggerConfig {
                                             RequestHandlerSelectors
                                             .basePackage("com.practicaintegradag7.controllers"))
                                     .paths(PathSelectors.any())
-                                    .build();
+                                    .build()
+                                    .apiInfo(getApiInfo());
                 }
+            
+            @SuppressWarnings("deprecation")
+			private ApiInfo getApiInfo() {
+        		return new ApiInfo(
+        				"SIGEVA API",
+        				"Documentación de la API de SIGEVA realizada con Swagger",
+        				"1.0",
+        				null, null, null, null)
+        				;
+        	}
 }
